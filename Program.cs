ab0b72bb7a77bbb064c38b172e405f5bd18be6f7
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-09-10 12:30:30 +0300

    Code for Second Array is written

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина
+// которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на
+// старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+// исключительно массивами.
+
 string[] CreateAnitialArray()
 {
     Console.Write("How many elements AnitialArray should consist of?:\t");
@@ -23,4 +27,46 @@ string[] CreateAnitialArray()
     return AnitialArray;
 }
 
-CreateAnitialArray();
+void SecondArrayLength(string[] AnitialArray)
+{
+    int n = 0;
+    for (int i = 0; i < AnitialArray.Length; i++)
+    {
+        if (AnitialArray[i].Length <= 3)
+            n++;
+    }
+
+    if (n < 1)
+    {
+        Console.WriteLine("There are no elements in AnitialArray to include in FinalArray");
+
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Length of FinalArray is {n} elements and they are following:");
+
+        string[] FinalArray = new string[n];
+
+
+        int j = 0;
+        for (int i = 0; i < AnitialArray.Length; i++)
+        {
+            if (AnitialArray[i].Length <= 3)
+            {
+                FinalArray[j] = AnitialArray[i];
+                j++;
+            }
+        }
+
+        for (int i = 0; i < FinalArray.Length; i++)
+        {
+            Console.Write($" {FinalArray[i]} |");
+        }
+        System.Console.WriteLine();
+        System.Console.WriteLine();
+    }
+
+}
+
+SecondArrayLength(CreateAnitialArray());
