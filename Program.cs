7d3c7f703c6d56e31ea289f3878dfd6f610295e1
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-09-10 12:07:06 +0300

    First array is filled in and printed out

--- Program.cs
+++ Program.cs
@@ -2,4 +2,22 @@
 // которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на
 // старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
 // исключительно массивами.
-Console.WriteLine("Hello, World!");
+Console.Write("How many elements AnitialArray should consist of?:\t");
+int AnitialArrayLenght = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+string[] AnitialArray = new string[AnitialArrayLenght];
+for (int i = 0; i < AnitialArray.Length; i++)
+{
+    Console.Write($"Please fill in {i + 1} element of AnitialArray :\t ");
+    AnitialArray[i] = Console.ReadLine();
+}
+Console.WriteLine();
+
+Console.WriteLine("AnitialArray consists of following elements:");
+for (int i = 0; i < AnitialArray.Length; i++)
+{
+    Console.Write($"{AnitialArray[i]} | ");
+}
+Console.WriteLine();
+Console.WriteLine("========================================================================");
