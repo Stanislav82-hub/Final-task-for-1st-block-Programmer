ea38bd896b10646cc2b969bdbbb0d77eaad92504
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-09-10 12:24:35 +0300

    Code for First Array is written

--- Program.cs
+++ Program.cs
@@ -1,61 +1,26 @@
-﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина
-// которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на
-// старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
-// исключительно массивами.
-
-Console.Write("How many elements AnitialArray should consist of?:\t");
-int AnitialArrayLenght = int.Parse(Console.ReadLine());
-Console.WriteLine();
-
-string[] AnitialArray = new string[AnitialArrayLenght];
-for (int i = 0; i < AnitialArray.Length; i++)
+﻿
+string[] CreateAnitialArray()
 {
-    Console.Write($"Please fill in {i + 1} element of AnitialArray :\t ");
-    AnitialArray[i] = Console.ReadLine();
-}
-Console.WriteLine();
-
-Console.WriteLine("AnitialArray consists of following elements:");
-for (int i = 0; i < AnitialArray.Length; i++)
-{
-    Console.Write($"{AnitialArray[i]} | ");
-}
-Console.WriteLine();
-Console.WriteLine("========================================================================");
-
-int n = 0;
-for (int i = 0; i < AnitialArray.Length; i++)
-{
-    if (AnitialArray[i].Length <= 3)
-        n++;
-}
-if (n < 1)
-{
-    Console.WriteLine("There are no elements in AnitialArray to include in FinalArray");
-
-}
-else
-{
+    Console.Write("How many elements AnitialArray should consist of?:\t");
+    int AnitialArrayLenght = int.Parse(Console.ReadLine());
     Console.WriteLine();
-    Console.WriteLine($"Length of FinalArray is {n} elements and they are following:");
 
-    string[] FinalArray = new string[n];
-
-
-    int j = 0;
+    string[] AnitialArray = new string[AnitialArrayLenght];
     for (int i = 0; i < AnitialArray.Length; i++)
     {
-        if (AnitialArray[i].Length <= 3)
-        {
-            FinalArray[j] = AnitialArray[i];
-            j++;
-        }
+        Console.Write($"Please fill in {i + 1} element of AnitialArray :\t ");
+        AnitialArray[i] = Console.ReadLine();
     }
+    Console.WriteLine();
 
-    for (int i = 0; i < FinalArray.Length; i++)
+    Console.WriteLine("AnitialArray consists of following elements:");
+    for (int i = 0; i < AnitialArray.Length; i++)
     {
-        Console.Write($" {FinalArray[i]} |");
+        Console.Write($"{AnitialArray[i]} | ");
     }
-    System.Console.WriteLine();
-    System.Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("========================================================================");
+    return AnitialArray;
 }
+
+CreateAnitialArray();
